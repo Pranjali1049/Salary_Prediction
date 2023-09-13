eb1adef9002f9ee01266a4bed6bdc5302021c526
Pranjali Sharma <pranjali.sharma1049@gmail.com>
2023-09-13 22:02:26 +0530

    Add files via upload

--- Salary Prediction.py
+++ Salary Prediction.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[36]:
+# <h1>Salary Prediction Model<h1>
+
+# 
+# The Salary Dataset contains **6704 rows** and **6 columns** containing the following data:
+# 
+# 1. **Age**
+# 2. **Gender**
+# 3. **Education Level**
+# 4. **Job Title**
+# 5. **Years of Experience**
+# 6. **Salary**
+# 
+# First we pre-process, clean and model the data to standarsise and structure it.
+# 
+
+# In[157]:
 
 
 # Importing Libraries
@@ -19,83 +34,522 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error,mean_absolute_error 
 
 
-# In[37]:
+# In[158]:
 
 
 # Importing Data
+
 df = pd.read_csv(r'C:\Users\acer\Downloads\Salary_Data.csv')
 df
 
 
-# In[38]:
+# In[159]:
 
 
 df.info()
 
 
-# In[39]:
+# In[160]:
 
 
 df.describe()
 
 
-# In[40]:
+# In[161]:
 
+
+# Checking for null data
 
 df.isnull().sum()
 
 
-# In[41]:
+# In[162]:
 
+
+# Dropping null values from database
 
 df.dropna(inplace=True)
 
 
-# In[42]:
+# In[163]:
 
+
+# Checking unique value counts of Job Titles in the database
 
 df['Job Title'].value_counts()
 
 
-# In[43]:
+# In[164]:
 
 
 # Reducing Job titles by omitting titles with less than 25 counts
+
 job_title_count = df['Job Title'].value_counts()
 job_title_edited = job_title_count[job_title_count<=25]
 job_title_edited.count()
 
 
-# In[44]:
+# In[165]:
 
 
-# Omitting titles with less than 20 counts
+# Omitting titles with less than 25 counts
+
 df['Job Title'] = df['Job Title'].apply(lambda x: 'Others' if x in job_title_edited else x )
 df['Job Title'].nunique()
 
 
-# In[45]:
+# In[166]:
 
+
+#Checking unique value count of Education Level
 
 df['Education Level'].value_counts()
 
 
-# In[46]:
+# In[167]:
 
 
 # Combining repeating values of education level
+
 df['Education Level'].replace(["Bachelor's Degree","Master's Degree","phD"],["Bachelor's","Master's","PhD"],inplace=True)
 df['Education Level'].value_counts()
 
 
-# In[47]:
+# In[168]:
 
+
+# Checking Unique Value count of Gender
 
 df['Gender'].value_counts()
 
 
-# In[ ]:
+# <h1>Exploratory Data Analysis<h1>
+
+# In[169]:
 
 
+# Create a figure with two subplots
+fig, ax = plt.subplots(1, 2, figsize=(15, 5))
+
+# Plot the count of data points for each gender category in the first subplot
+sns.countplot(x='Gender', data=df, ax=ax[0])
+
+# Plot the count of data points for each education level category in the second subplot
+sns.countplot(x='Education Level', data=df, ax=ax[1])
+
+# Add labels and titles for the subplots
+ax[0].set_xlabel('Gender')
+ax[0].set_ylabel('Count')
+ax[0].set_title('Distribution of Gender')
+
+ax[1].set_xlabel('Education Level')
+ax[1].set_ylabel('Count')
+ax[1].set_title('Distribution of Education Level')
+
+# Show the plots
+plt.tight_layout()
+plt.show()
 
 
+# **Distribution of Gender and Education Level**
+#     
+# 1. **Distribution of Gender:** The job market is mostly dominated by males with females being second largest group and extremely few people from the third gender. This shows that while women are slowly becoming a part of the workforce, people belonging to other genders do have very few opportunities.
+#     
+# 2. **Distribution of Education Level:** A majority of working professionals hold a bachelor's degree followed by master's and PhD holders with least hireable being just High school graduates. This means a bachelor's degree is sufficient to enter the workforce and highly in demand by employers.
+# 
+
+# In[170]:
+
+
+# Get the top 10 job titles with the highest mean salary
+top_10_highest_paying_jobs = df.groupby('Job Title')['Salary'].mean().nlargest(10)
+
+# Create a single bar plot for the top 10 highest paying job titles and their mean salaries
+plt.figure(figsize=(12, 8))
+sns.barplot(x=top_10_highest_paying_jobs.index, y=top_10_highest_paying_jobs.values)
+
+# Customize the plot
+plt.xlabel('Job Title')
+plt.ylabel('Mean Salary')
+plt.title('Top 10 Highest Paying Jobs')
+plt.xticks(rotation=60)  # Rotate x-axis labels if needed
+
+# Show the plot
+plt.tight_layout()
+plt.show()
+
+
+# **Top 10 Highest Paying Jobs**
+#     
+# The plot shows the Top 10 highest paying jobs in the industry plotted against the mean salary offered for the role. We can see that Data Scientists and Engineers are paid the highest.
+
+# In[173]:
+
+
+# Create a figure with three subplots
+fig, ax = plt.subplots(3, 1, figsize=(12, 15))
+
+# Create a histogram of Age in the first subplot
+sns.histplot(df['Age'], ax=ax[0], color='blue', kde=True)
+ax[0].set_title('Age Distribution')
+ax[0].set_xlabel('Age')
+
+# Create a histogram of Years of Experience in the second subplot
+sns.histplot(df['Years of Experience'], ax=ax[1], color='orange', kde=True)
+ax[1].set_title('Years of Experience Distribution')
+ax[1].set_xlabel('Years of Experience')
+
+# Create a histogram of Salary in the third subplot
+sns.histplot(df['Salary'], ax=ax[2], color='green', kde=True)
+ax[2].set_title('Salary Distribution')
+ax[2].set_xlabel('Salary')
+
+plt.tight_layout()
+plt.show()
+
+
+# **Distribution of continuous variables**
+#     
+# 1. **Age Distribution:** It shows that majority of workforce lies in the 27-31 age range which suggests a highly youthful workforce.
+#     
+# 2. **Years of Experience Distribution:** The plot shows that maximum workforce has 1-4 years of experience which is in agreement with previous results. Younger workforce has less experience than older counterparts
+#     
+# 3. **Salary Distribution:** The salary majority of workers earn lies in 50,000-60,000 dollars range while similar count of people earn 1,80,000 as well. The plot reveals that there is a lot of different pay ranges within the community with majority of workers earning less than 1,25,000 dollars yearly.
+# 
+
+# In[174]:
+
+
+# Create a figure with two subplots
+fig, ax = plt.subplots(1, 2, figsize=(15, 5))
+
+# Create a bar plot for the relationship between Gender and Salary in the first subplot
+sns.barplot(x='Gender', y='Salary', data=df, ax=ax[0])
+ax[0].set(title='Relationship between Gender and Salary', xlabel='Gender', ylabel='Mean Salary')
+
+# Create a box plot for the relationship between Education Level and Salary in the second subplot
+sns.boxplot(x='Education Level', y='Salary', data=df, ax=ax[1])
+ax[1].set(title='Relationship between Education Level and Salary', xlabel='Education Level', ylabel='Salary')
+
+# Rotate x-axis labels in the second subplot for better readability
+ax[1].tick_params(axis='x', rotation=45)
+
+# Adjust spacing between subplots
+plt.tight_layout()
+
+# Show the plots
+plt.show()
+
+
+# **Relationship between discreet variables**
+#     
+# 1. **Gender and Salary:** Other genders earn more than men and women. Women earn the least out of all three genders.
+#     
+# 2. **Education Level and Salary:** The plot reveals that as your education level rises, the mean salary level also rises. High school graduates are paid the least and PhD's the highest. The box plot also reveals the range of salaries and it is highest for bacahelr's degree meaning the distribution of salaries is more diverse for bachelor's degree with some earning as much as somone with a Master's degree would.
+#     
+
+# In[134]:
+
+
+sns.barplot(x='Education Level',y='Salary',data=df,hue='Gender').set(title='Education level vs Salary vs Gender')
+plt.show()
+
+
+# **Relationship between Education Level, Salary and Gender**
+# 
+# Following trends from previous charts, men on an average are paid more than women with same education level. Where the third gender is present, they are paid more than both the genders. Also, the more you are educated, the more you are paid.
+
+# In[135]:
+
+
+# Create a figure with two subplots
+fig, ax = plt.subplots(1, 2, figsize=(15, 5))
+
+# Scatter plot for relationship between age and salary
+sns.regplot(x='Age',y='Salary',data=df, scatter_kws={'color':'orange'}, line_kws={'color':'green'}, ax=ax[0])
+ax[0].set(title='Relationship between Age and Salary')
+
+# Scatter plot for relationship between experience and salary
+sns.regplot(x='Years of Experience',y='Salary',data=df, scatter_kws={'color':'red'}, line_kws={'color':'blue'}, ax=ax[1])
+ax[1].set(title='Relationship between Experience and salary')
+
+plt.tight_layout()
+plt.show()
+
+
+# 1. **Relationship between Age and Salary:** The is strong positive correlation between age and salary meaning older employees get paid more than younger e,ployees.
+# 
+# 2. **Relationship between Experience and Salary:** There is strong positive correlation between Experience and salary meaning experienced candidates get paid more. The more experience a candidate gains, the more their salary will become.
+# 
+
+# In[136]:
+
+
+# Mapping Education Level column
+education_mapping = {"High School": 0, "Bachelor's": 1, "Master's": 2, "PhD": 3}
+df['Education Level'] = df['Education Level'].map(education_mapping)
+
+# Label encoding the categorical variable
+le = LabelEncoder()
+df['Gender'] = le.fit_transform(df['Gender'])
+
+# Correlation plot
+plt.figure(figsize=(10, 8))  # Set the figure size
+sns.heatmap(df.corr(), annot=True, cmap='coolwarm')  # Use cmap for color map
+plt.title('Correlation Plot')
+plt.xticks(rotation=60) 
+plt.yticks(rotation=60) 
+plt.show()
+
+
+# **Heatmap**
+# 
+# The heatmap reveals the degree of correlation between the variables
+# 
+# 1. Highest correlation between age and years of experience
+# 2. High correlation between salary and years of experience
+# 3. High correlation between salary and age
+# 4. Moderately high correlation between salary and education level
+# 5. Low correlation between gender and all variables
+# 
+
+# **Predicting Salary**
+# 
+# 3 Models will be used to predict the salary
+# 
+# 1. Linear Regression
+# 2. Deision Tree
+# 3. Random Forest
+
+# In[137]:
+
+
+# detecting the outliers in salary column using IQR method
+Q1 = df.Salary.quantile(0.25) # First Quartile
+Q3 = df.Salary.quantile(0.75) # Third Quartile
+
+# Caltulation Interquartile
+IQR = Q3-Q1
+
+# Deetecting outliers lying 1.5x of IQR above and below Q1 and Q3 resp
+lower = Q1-1.5*IQR
+upper = Q3+1.5*IQR
+
+
+# In[138]:
+
+
+df[df.Salary>upper]
+
+
+# **No outliers in Q3**
+
+# In[139]:
+
+
+df[df.Salary<lower]
+
+
+# **No outliers in Q1**
+
+# <h2>Preparing the data for ML analysis by converting categorical job titles into a numerical format<h2>
+
+# In[140]:
+
+
+# Creating dummies for Job titles
+dummies = pd.get_dummies(df['Job Title'],drop_first=True)
+df = pd.concat([df,dummies],axis=1)
+
+# Drop Job Title column
+df.drop('Job Title',inplace=True,axis=1)
+df.head()
+
+
+# In[141]:
+
+
+# Separating the dataset into features and target
+
+# Dataset conntaining all features from df
+features = df.drop('Salary',axis=1)
+
+# Series containing target variable to be predicted
+target = df['Salary']
+
+
+# In[142]:
+
+
+# Splitting data into 25% training and 75% test sets
+
+x_train,x_test,y_train,y_test = train_test_split(features,target,test_size=0.25,random_state=42)
+x_train.shape
+
+
+# In[143]:
+
+
+# Create a dictionary for defining models and tuning hyperparameters
+
+model_params = {
+    'Linear_Regression':{
+        'model':LinearRegression(),
+        'params':{
+            
+        }
+    },
+    'Decision_Tree':{
+        'model':DecisionTreeRegressor(),
+        'params':{
+            'max_depth':[2,4,6,8,10],
+            'random_state':[0,42],
+            'min_samples_split':[1,5,10,20]
+        }
+    },
+    'Random_Forest':{
+        'model':RandomForestRegressor(),
+        'params':{
+            'n_estimators':[10,30,20,50,80]
+        }
+    }
+}
+
+
+# In[144]:
+
+
+# Hyper parameter tuning through grid search cv
+score=[]
+
+for model_name,m in model_params.items():
+    clf = GridSearchCV(m['model'],m['params'],cv=5,scoring='neg_mean_squared_error')
+    clf.fit(x_train,y_train)
+    
+    score.append({
+        'Model':model_name,
+        'Params':clf.best_params_,
+        'MSE(-ve)':clf.best_score_
+    })
+pd.DataFrame(score)    
+
+
+# In[145]:
+
+
+# Order of the best models 
+
+s = pd.DataFrame(score)
+sort = s.sort_values(by = 'MSE(-ve)',ascending=False)
+sort
+
+
+# In[146]:
+
+
+# Random Forest model
+
+rfr = RandomForestRegressor(n_estimators=20)
+rfr.fit(x_train,y_train)
+
+
+# In[147]:
+
+
+rfr.score(x_test,y_test)
+
+
+# In[148]:
+
+
+y_pred_rfr = rfr.predict(x_test)
+
+print("Mean Squared Error :",mean_squared_error(y_test,y_pred_rfr))
+print("Mean Absolute Error :",mean_absolute_error(y_test,y_pred_rfr))
+print("Root Mean Squared Error :",mean_squared_error(y_test,y_pred_rfr,squared=False))
+
+
+# In[150]:
+
+
+# Decision Tree model
+
+dtr = DecisionTreeRegressor(max_depth=10,min_samples_split=2,random_state=0)
+dtr.fit(x_train,y_train)
+
+
+# In[151]:
+
+
+dtr.score(x_test,y_test)
+
+
+# In[152]:
+
+
+y_pred_dtr = dtr.predict(x_test)
+
+print("Mean Squared Error :",mean_squared_error(y_test,y_pred_dtr))
+print("Mean Absolute Error :",mean_absolute_error(y_test,y_pred_dtr))
+print("Root Mean Squared Error :",mean_squared_error(y_test,y_pred_dtr,squared=False))
+
+
+# In[153]:
+
+
+# Linear regression model
+
+lr = LinearRegression()
+lr.fit(x_train,y_train)
+
+
+# In[154]:
+
+
+lr.score(x_test,y_test)
+
+
+# In[155]:
+
+
+y_pred_lr = lr.predict(x_test)
+
+print("Mean Squared Error :",mean_squared_error(y_test,y_pred_lr))
+print("Mean Absolute Error :",mean_absolute_error(y_test,y_pred_lr))
+print("Root Mean Squared Error :",mean_squared_error(y_test,y_pred_lr,squared=False))
+
+
+# In[176]:
+
+
+# Access the feature importances of Random Forest Regressor
+feature_importances = rfr.feature_importances_
+
+# Assuming you have a list of feature names that corresponds to the feature importances
+feature_names = list(x_train.columns)
+
+# Sort feature importances in descending order
+sorted_indices = np.argsort(feature_importances)[::-1]
+sorted_feature_importances = [feature_importances[i] for i in sorted_indices]
+sorted_feature_names = [feature_names[i] for i in sorted_indices]
+
+# Create a bar chart
+plt.figure(figsize=(12, 8))
+plt.barh(sorted_feature_names[:10], sorted_feature_importances[:10])
+plt.xlabel('Feature Importance')
+plt.title('Top 10 Feature Importance in Predicting Salary')
+plt.gca().invert_yaxis()  # Invert the y-axis for better visualization
+plt.show()
+
+
+# **A bar chart depicting the importance of different features in predicting salary.**
+
+# <h2>Conclusion<h2>
+
+# **1. The Random Forest model achieved the highest R-squared score (0.971) and the lowest MSE, MAE, and RMSE values, indicating the best predictive performance among the three models.**
+# 
+# **2. The Decision Tree model performed well with an R-squared score of 0.941 but had higher errors compared to the Random Forest.**
+# 
+# **3. The Linear Regression model had the lowest R-squared score (0.833) and the highest errors, suggesting it may not capture the underlying patterns in the data as effectively as the ensemble models.**
+# 
+# In conclusion, the Random Forest model appears to be the most suitable for predicting salaries in this dataset, as it offers the highest predictive accuracy and the lowest error metrics. Further optimization and fine-tuning of the Random Forest model could potentially lead to even better results.
